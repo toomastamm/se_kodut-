026d9fbbb3bc10baf1ae1b20ce64f8db4a745777
Toomas Tamm <toomastmm@gmail.com>
2018-11-12 13:12:22 +0200

    Toote leht uuendab lehe pealkirja vastavalt tootele.

--- toode.js
+++ toode.js
@@ -3,13 +3,17 @@ const id = parameetrid.get('id');
 
 if (id == null) {
     muuda_nähtavust("toode");
+    document.title = "Viga - Mingi Pood";
 } else {
     let toode = toode_IDst(tooted, id);
 
     if (toode === false) {
         muuda_nähtavust("toode");
+        document.title = "Viga - Mingi Pood";
+
     } else {
         muuda_nähtavust("error");
+        document.title = toode["nimi"] + " - Mingi Pood";
         document.getElementById("nimi").innerHTML = toode["nimi"];
         document.getElementById("hind").innerHTML = "Osta kohe " + toode["hind"] + "€";
         document.getElementById("kirjeldus").innerHTML = dict_tabeliks(toode["specs"]);
