5faaf1b1dd382d3c350f3830ec95e7740cfc2b20
Toomas Tamm <toomastmm@gmail.com>
2018-11-21 15:41:01 +0200

    Parandatud lehe nimed javascriptis

--- toode.js
+++ toode.js
@@ -3,17 +3,17 @@ const id = parameetrid.get('id');
 
 if (id == null) {
     muuda_nähtavust("toode");
-    document.title = "Viga - Mingi Pood";
+    document.title = "Viga - Macro Center";
 } else {
     let toode = toode_IDst(tooted, parseInt(id));
 
     if (toode === false) {
         muuda_nähtavust("toode");
-        document.title = "Viga - Mingi Pood";
+        document.title = "Viga - Macro Center";
 
     } else {
         muuda_nähtavust("error");
-        document.title = toode["nimi"] + " - Mingi Pood";
+        document.title = toode["nimi"] + " - Macro Center";
         document.getElementById("nimi").innerHTML = toode["nimi"];
         document.getElementById("hind").innerHTML = toode["hind"];
         document.getElementById("kirjeldus").innerHTML = dict_tabeliks(toode["specs"]);
