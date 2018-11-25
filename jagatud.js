68710a650fdd9538fccf68c71bcbb4c66d8b6267
Toomas Tamm <toomastmm@gmail.com>
2018-11-25 15:56:39 +0200

    Eemaldatud ebavajalikud read

--- jagatud.js
+++ jagatud.js
@@ -49,15 +49,13 @@ function dict_tabeliks(dict) {
 }
 
 function html_tootelist(tooted, sihtmärk, template) {
-    let flex = "";
     sihtmärk.innerHTML = "";
     for (let i = 0; i < tooted.length; i++) {
-        toode = document.importNode(template.content, true);
+        let toode = document.importNode(template.content, true);
         toode.querySelector("#pilt").src = tooted[i]["pilt"];
         toode.querySelector("#nimi").textContent = tooted[i]["nimi"];
-        toode.querySelector("#hind").textContent = tooted[i]["hind"] +  "€";
+        toode.querySelector("#hind").textContent = tooted[i]["hind"] + "€";
         toode.querySelector("#hind").href = "toode.html?id=" + tooted[i]["id"];
         sihtmärk.appendChild(toode);
-        console.log(toode)
     }
 }
