c2fdba1e0470fecc60f19648f0b547099537455c
Toomas Tamm <toomastmm@gmail.com>
2018-11-25 16:32:41 +0200

    Erinevad lehe osad alustavad nüüd peidetult, sujuvam laadimine

--- toode.js
+++ toode.js
@@ -5,22 +5,22 @@ const template_tabel = document.getElementById("tabel");
 const template_veerg = document.getElementById("veerg");
 
 if (id == null) {
-    muuda_nähtavust("toode");
+    muuda_nähtavust("error");
     document.title = "Viga - Macro Center";
 } else {
     let toode = toode_IDst(tooted, parseInt(id));
 
     if (toode === false) {
-        muuda_nähtavust("toode");
+        muuda_nähtavust("error");
         document.title = "Viga - Macro Center";
 
     } else {
-        muuda_nähtavust("error");
         document.title = toode["nimi"] + " - Macro Center";
         document.getElementById("nimi").innerHTML = toode["nimi"];
         document.getElementById("hind").innerHTML = toode["hind"];
         document.getElementById("pilt").src = toode["pilt"];
         html_speclist(toode["specs"], html_toode_kirjeldus, template_tabel, template_veerg);
+        muuda_nähtavust("toode");
     }
 }
 
