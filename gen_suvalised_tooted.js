195eaa575b0c3d1497fbc83e57f7e297307821d9
Toomas Tamm <toomastmm@gmail.com>
2018-11-27 09:13:35 +0200

    Rohkem erinevusi tootete vahel, genereerib mitu korda kiiremini

--- gen_suvalised_tooted.js
+++ gen_suvalised_tooted.js
@@ -1,10 +1,10 @@
-tooted = [];
+let tooted = [];
 
 function suvaline_number(min, max) {
     return Math.floor(Math.random() * (max - min)) + min
 }
 
-function suvalised_listist(list, protsent) {
+function suvalised_listist(list, protsent, eraldaja = ', ') {
     let uus_list = [];
     for (let i = 0; i < list.length; i++) {
         let x = Math.random();
@@ -13,7 +13,7 @@ function suvalised_listist(list, protsent) {
         }
     }
 
-    return uus_list
+    return uus_list.join(eraldaja)
 }
 
 function suvaline_listist(list) {
@@ -21,9 +21,10 @@ function suvaline_listist(list) {
 }
 
 function gen_ram(id) {
-    let gb = suvaline_number(1, 32);
+    let gb = suvaline_listist([1, 2, 4, 6, 8, 12, 16, 18, 24, 28, 32, 64, 128]);
+    let tootja = suvaline_listist(["Kingston", "A-Data", "Crosair", "Crucial", "Dell", "Goodram", "G.SKILL", "Samsung", "Silicon Power"]);
     let ddr = suvaline_number(1, 4);
-    let nimi = gb + "GB DDR" + ddr;
+    let nimi = tootja + " " + gb + "GB DDR" + ddr;
     return {
         "id": id,
         "type": "ram",
@@ -32,7 +33,7 @@ function gen_ram(id) {
         "pilt": "pildid/ram.png",
         "specs": {
             "ÜLDNÄITAJAD": {
-                "Tootja": suvaline_listist(["SAMSING", "CRUCAL", "RIPEJAW"]),
+                "Tootja": tootja,
                 "Nimi": nimi,
                 "Moodulite arv": suvaline_listist([1, 2, 4, 8]),
             },
@@ -40,35 +41,82 @@ function gen_ram(id) {
                 "Maht": gb + "GB",
                 "Tüüp": "DDR" + ddr,
                 "Voltage": suvaline_listist(["1.2v LOW VOLTAGE", "1.5v"]),
+                "Siini kiirus": suvaline_number(10, 24) * 100 + "MHz",
             },
             "ERIMÄRKUSED": {
-                "Lisad": suvalised_listist(["Anti-Static band", "Protective plastic", "Bubblewrap", "-10% off coupon"], 40),
+                "Lisad": suvalised_listist(["Anti-Static band", "NON-ECC unbuffered", "Heat sink", "EEC", "RGB LED", "Overclock enabled"], 60),
             }
         },
     }
 }
 
-function gen_cpu(id) {
-    let nimi = "j" + suvaline_number(1, 9) + "-" + suvaline_number(1000, 9999) + suvalised_listist(["k", "h", "q", "m"], 25);
+function gen_intel_cpu(id) {
+    let edition = suvalised_listist(["k", "h", "q", "m"], 25, "");
+    let tüüp = "i" + suvaline_listist([3, 5, 7, 9]);
+    let mudel = suvaline_number(1000, 9999);
+    let nimi = "Intel " + tüüp + "-" + mudel + edition;
     return {
         "id": id,
         "type": "cpu",
         "nimi": nimi,
-        "hind": suvaline_number(100, 5000),
+        "hind": suvaline_number(300, 2000),
         "pilt": "pildid/cpu.png",
         "specs": {
             "ÜLDNÄITAJAD": {
-                "Tootja": "jntel",
+                "Tootja": "intel",
                 "Nimi": nimi,
-                "Tüüp": nimi.substr(0, 2),
+                "Tüüp": tüüp,
+                "Mudel": mudel + edition,
             },
             "TEHINILISED NÄITAJAD": {
                 "CPU CLOCK": suvaline_number(10, 36) / 10 + "GHz",
                 "CORE COUNT": suvaline_listist([1, 2, 4, 8, 16]),
                 "SOCKET": suvaline_number(1000, 9999) + "S",
             },
+            "ERIMÄRKUSED": {
+                "Lisad": suvalised_listist(["Overclockable", "Heat sink", "Hyperthreading", "VT-x", "Speedstep", "AES", "VR READY"], 60),
+            }
+
         },
     }
+}
+
+function gen_amd_cpu(id) {
+    let edition = suvaline_listist([3, 5, 7]);
+    let pro = suvaline_listist(["Pro ", ""]);
+    let generation = (suvaline_number(1, 2) * 1000);
+    let model = suvaline_number(0, 9) * 100;
+    let X = suvaline_listist(["X", ""]);
+    let nimi = "AMD Ryzen " + edition + " " + pro + "" + (generation + (model) + X);
+    return {
+        "id": id,
+        "type": "cpu",
+        "nimi": nimi,
+        "hind": suvaline_number(100, 600),
+        "pilt": "pildid/cpu.png",
+        "specs": {
+            "ÜLDNÄITAJAD": {
+                "Tootja": "AMD",
+                "Nimi": nimi,
+                "Tüüp": "Ryzen" + edition,
+                "Mudel": (generation + model) + X,
+            },
+            "TEHINILISED NÄITAJAD": {
+                "CPU CLOCK": suvaline_number(10, 36) / 10 + "GHz",
+                "CORE COUNT": suvaline_listist([1, 2, 4, 8, 16]),
+                "SOCKET": suvaline_number(1000, 9999) + "S",
+            },
+            "ERIMÄRKUSED": {
+                "Lisad": suvalised_listist(["Overclockable", "Heat sink", "SMT", "AES", "AVX2", "FMA3", "VR READY"], 60),
+            }
+
+        },
+    }
+}
+
+function gen_cpu(id) {
+    let random = suvaline_listist(["gen_amd_cpu(id)", "gen_intel_cpu(id)"]);
+    return eval(random)
 }
 
 function gen_gpu(id) {
@@ -117,7 +165,8 @@ function gen_gpu(id) {
 }
 
 for (let i = 0; i < 100; i++) {
-    tooted.push(suvaline_listist([gen_gpu(i), gen_cpu(i), gen_ram((i))]))
+    let random = suvaline_listist(["gen_gpu(i)", "gen_cpu(i)", "gen_ram((i))"]);
+    tooted.push(eval(random))
 }
 
 console.log(tooted);
